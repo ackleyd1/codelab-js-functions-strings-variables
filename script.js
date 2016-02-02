d2a55369a904bf5daa3d4278010af53dfb4c92d3
David Ackley <ackleyd1@msu.edu>
2016-02-02 14:41:18 -0500

    Fixed minor issues

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 var name = prompt("What is your name?");
 document.getElementById('name').textContent = name;
 var color = prompt("What is your favorite color?");
-document.getElementById('color').textContent= color;
+document.getElementById('color').textContent = color;
 var food = prompt("What is your favorite food?");
 document.getElementById('food').textContent = food;
-document.getElementById('div').style.background=color;
+document.getElementById('div').style.background = color;
